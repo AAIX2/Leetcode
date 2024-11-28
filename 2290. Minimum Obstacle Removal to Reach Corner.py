8d88632b48f1e6f076d960d175cd40e0a7e283b1
Abhinav <abhinavnarula052@gmail.com>
2024-11-28 20:11:49 +0530

    Updated 2290. Minimum Obstacle Removal to Reach Corner

--- 2290. Minimum Obstacle Removal to Reach Corner.py
+++ 2290. Minimum Obstacle Removal to Reach Corner.py
@@ -29,3 +29,32 @@ class Solution:
             
 # Time Complexity- O(E log V) or O(n*mlog(n*m)) since at the worst case edges can be n*m and vertices are n*m
 # Space Complexity- O(n*m)
+
+
+# Using 0-1 BFS
+
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        dir_x = [0,0,-1,1]
+        dir_y = [-1,1,0,0]
+        obs = [[float('inf') for i in range(m)]for i in range(n)]
+        q = deque([[0,0,0]])
+        obs[0][0] = 0
+        while q:
+            i,j,obstacles = q.popleft()
+            for d in range(4):
+                new_x,new_y = i+dir_x[d],j+dir_y[d]
+                if new_x>=0 and new_x<n and new_y>=0 and new_y<m and obs[new_x][new_y] == float('inf'):
+                    if grid[new_x][new_y] == 1:
+                        obs[new_x][new_y] = 1+obstacles
+                        q.append([new_x,new_y,1+obstacles])
+                    else:
+                        obs[new_x][new_y] = obstacles
+                        q.appendleft([new_x,new_y,obstacles])
+            
+        return obs[n-1][m-1]
+
+# Time Complexity- O(n*m)
+# Space Complexity- O(n*m)
