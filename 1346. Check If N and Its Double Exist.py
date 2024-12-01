5df660479b9478eeab90f10f9d922b4290fbd4fe
Abhinav <abhinavnarula052@gmail.com>
2024-12-02 02:10:38 +0530

    Added a solution for 1346. Check If N and Its Double Exist

---
+++ 1346. Check If N and Its Double Exist.py
@@ -0,0 +1,15 @@
+# Using a hash set
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        s = set()
+        for n in arr:
+            if 2*n in s or (n%2 == 0 and n//2 in s):
+                return True
+            s.add(n)
+        return False
+# Time Complecity- O(n)
+# Space Complexity- O(n)
+
+
+
