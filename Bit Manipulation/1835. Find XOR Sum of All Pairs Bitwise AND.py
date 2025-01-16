171034db2f67129a4c1c9c3027d92564d5f594ff
abhinav <abhinavnarula052@gmail.com>
2025-01-16 20:20:32 +0530

    Added solution for 1835. Find XOR Sum of All Pairs Bitwise AND

---
+++ Bit Manipulation/1835. Find XOR Sum of All Pairs Bitwise AND.py
@@ -0,0 +1,16 @@
+# Approach (using observation)
+# T.C : O(n+m)
+# S.C : O(1)
+
+
+class Solution:
+    def getXORSum(self, arr1: List[int], arr2: List[int]) -> int:
+        n = len(arr1)
+        m = len(arr2)
+        xor1 = 0
+        for num in arr1:
+            xor1^=num
+        xor2 = 0
+        for num in arr2:
+            xor2^=num
+        return xor1&xor2
